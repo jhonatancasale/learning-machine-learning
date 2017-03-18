391676ea49b0fc1aff9af356a7e60388434f24ff
jhonatancasale <jhonatan@jhonatancasale.com>
2017-03-18 03:40:12 -0300

    Refact using a Class Perceptron

--- techniques/ANN/perceptron/self_made/python/perceptron.py
+++ techniques/ANN/perceptron/self_made/python/perceptron.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!env python3
 # -*- coding: utf-8 -*-
 
-"""
+'''
 # Author: Jhonatan Casale (jhc)
 #
 # Contact : jhonatan@jhonatancasale.com
@@ -11,157 +11,172 @@
 #         : http://jhonatancasale.github.io/
 #
 # Create date Wed  8 Mar 13:32:21 BRT 2017
-"""
+'''
 
 
 from glob import glob as ls
 from numpy import array, dot, random
-import matplotlib.pyplot as plt
 
 
-def f_activation(input_value, threshold=.5):
-    """
-    (double, double) -> int
-
-    Calculate when the neuron fires
-    """
-
-
-    return 0 if input_value < threshold else 1
-
-
-def build_dataset(filename):
-    """
-    str -> list
-
-    TODO Update me!!!!!
-    Return the content of `filename` in a numpy.matrix object
-    """
+def split_data(filename: str) -> (list, list, list, list):
+    '''
+    Split the content of `filename` in 2 datasets for training, and another
+    2 datasets to test the predictions.
+    '''
 
 
     print("Building dataset from {}".format(filename))
-    dataset = []
+    features_train = []
+    features_test = []
+    labels_train = []
+    labels_test = []
+
     with open(filename, "r") as _file:
         next(_file) # skip header
         for line in _file:
             values = [float(field) for field in line.split()]
             # add [1.0] to represent theta value
-            dataset.append((array(values[:-1] + [1.0]), values[-1]))
-        return dataset
+            features_train.append(array(values[:-1] + [1.0]))
+            labels_train.append(values[-1])
+        features_test = features_train[:]
+        labels_test = labels_train[:]
+        return features_train, features_test, labels_train, labels_test
 
 
-def train(training_set, learning_rate=.1, max_iterations=1e4, error=1e-2):
-    """
-    (list [, double] [, int] [, int]) -> (list, list)
-
-    Operate over the training set until the cost function produces an error
-    lesser than the (optional) given param `error`. In this case, the function
-    returns a tuple with `weights` adjusted values and a list containing (0,
-    100] computed average errors per iteration
-
-    Otherwise, return a tuple with None and the list with the first 100 average
-    errors calculated
-    """
+def print_value_table(labels_test: list, prediction: list) -> None:
+    '''
+    Prints on the screen the `expected value` followed by the `obtained value`
+    '''
 
 
-    # training_set[0] is a tuple, training_set[0][0] is an array
-    weights = array(random.random(training_set[0][0].size))
-
-    squared_error_list = []
-    for i in range(int(max_iterations)):
-        squared_error = 0
-        for sample, expected_output in training_set:
-            diff = expected_output - f_activation(dot(sample, weights))
-            squared_error += diff ** 2
-            weights = weights - learning_rate * (2 * diff * -sample)
-        squared_error /= len(training_set)
-        if i < 100:
-            squared_error_list.append(squared_error)
-        if squared_error < error:
-            return (weights, squared_error_list)
-    return (None, squared_error_list)
+    print("Expected\tObtained")
+    for label, pred in zip(labels_test, prediction):
+        print("{:<8}\t{}".format(label,pred))
 
 
-def print_value_table(dataset, weights):
-    """
-    (list, list) -> None
+def test_case(case: str) -> float:
+    '''
+    Open the file with the given name `case` and parse, split into datasets,
+    train the perceptron.
 
-    Prints on stdout a table containing both expected values and obtained
-    values from each dataset entry
-
-    Otherwise, just print `Convergence failure`
-    """
+    Returning the value of accuracy obtained after the training process
+    '''
 
 
-    if weights is not None:
-        print("Expected\tObtained")
-        for sample, expected_output in dataset:
-            print("{:<8}\t{}".
-                  format(expected_output,
-                         f_activation(dot(sample, weights)))
-                 )
-    else:
-        print("Convergence failure")
+    clf = Perceptron()
 
-
-def converged(dataset, weights):
-    """
-    (list, list) -> bool
-
-    Checks if the expected output from each entry from the given param
-    `dataset` are equal to the obtained output using the given param `weights`
-    """
-
-
-    if weights is None:
-        return False
-    for sample, expected_output in dataset:
-        if expected_output != f_activation(dot(sample, weights)):
-            return False
-    return True
-
-
-def plot_errors(errors):
-    """
-    (list) -> matplotlib.pyplot
-
-    Produces a plot of the given param `errors`
-    """
-
-
-    plt.close()
-    plt.xlabel("$Samples$")
-    plt.ylabel("$Value$")
-    plt.plot(errors, "b{}-".format("o" if len(errors) < 25 else ""),
-             label="Squared Error in the first 100 samples")
-    plt.legend(loc="upper right")
-    plt.axis([-.1, len(errors), -.1, max(errors) + .5])
-    plt.grid(True)
-    plt.show()
+    features_train, features_test, labels_train, labels_test = split_data(case)
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    return accuracy_score(labels_test, pred)
 
 
 def main():
-    """
+    '''
     Assumptions: input files *.dat on same dir
     For each *.dat file in the same dir, build a dataset for then and train
     the perceptron reporting the results
-    """
+    '''
 
-    files = ls("*.dat")
-    for case in files:
-        dataset = build_dataset(case)
-
-        weights, errors = train(dataset)
-        print("Training convergence process: {}{}".
-              format("Succeed" if converged(dataset, weights) else "Failed",
-                     "\n" if case != files[-1] else ""
-                    )
-             )
+    for case in ls("*.dat"):
+        print('Accuracy: {}%'.format(100 * test_case(case)))
 
 
-        #TODO parse plot command line option
-        if not converged(dataset, weights):
-            plot_errors(errors)
+    #TODO parse plot command line option
+    #if not converged(dataset, weights):
+    #    plot_errors(errors)
+
+###############################################################################
+
+
+def accuracy_score(labels_test: list, prediction: list) -> float:
+    '''
+    Return the calculated accuracy score, in other words, the number of `right`
+    predictions divided by `all` predictions
+    '''
+
+
+    if len(labels_test) != len(prediction):
+        raise ValueError('Both `labels_test` and `predictions` must have \
+                         the same length')
+
+    test_samples = zip(prediction, labels_test)
+    return sum([1 for p, l in  test_samples if p == l]) / len(labels_test)
+
+
+class Perceptron(object):
+    '''
+    error_rows could be parametrized
+    '''
+
+    def __init__(self: object):
+        self.weights = []
+        self.errors = []
+        self.converged= False
+
+
+    def weights(self: object) -> list:
+        return self.weights
+
+
+    def fit(self: object, features_train: list, labels_train: list,
+            learning_rate=.1, max_iterations=int(1e4), error=1e-2) -> None:
+        '''
+        (list [, double] [, int] [, int]) -> (list, list)
+
+        Operate over the training set until the cost function produces an error
+        lesser than the (optional) given param `error`. In this case, the
+        function returns a tuple with `weights` adjusted values and a list
+        containing (0, 100] computed average errors per iteration
+
+        Otherwise, return a tuple with None and the list with the first 100
+        average errors calculated
+        '''
+        self.converged = False
+        self.errors = []
+        self.weights = array(random.random(features_train[0].size))
+
+        for iteration in range(max_iterations):
+            squared_error = 0
+            for features, label in zip(features_train, labels_train):
+                #diff = label - f_activation(dot(features, self.weights))
+                diff = label - self.net_output(features)
+                squared_error += diff ** 2
+                self.weights = self.weights - learning_rate * (2 * diff * -features)
+            if iteration < 100:
+                self.errors.append(squared_error)
+            if squared_error / len(features) < error:
+                self.converged = True
+                break
+
+
+    def predict(self: object, features_test: list) -> list:
+        return [self.net_output(features) for features in features_test]
+
+
+    def net_output(self: object, feature_sample: list) -> float:
+        return self.f_activation(dot(feature_sample, self.weights))
+
+
+    def plot_errors(self: object) -> None:
+        import matplotlib.pyplot as plt
+
+
+        plt.close()
+        plt.xlabel("$Samples$")
+        plt.ylabel("$Value$")
+        plt.plot(
+            self.errors, "b{}-".format("o" if len(self.errors) < 25 else ""),
+            label="Squared Error in the first {} samples".format(self.errors)
+        )
+        plt.legend(loc="upper right")
+        plt.axis([-.1, len(self.errors), -.1, max(self.errors) + .5])
+        plt.grid(True)
+        plt.show()
+
+
+    def f_activation(self: object, input_value: float, threshold=.5) -> int:
+        return 0 if input_value < threshold else 1
 
 
 if __name__ == '__main__':
